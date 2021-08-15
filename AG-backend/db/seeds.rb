6d3f085ac48e58ab7d3840498b48074161f0b91b
bambi teague <teeegsa@gmail.com>
2021-08-15 12:48:20 -0500

    seed image data

--- AG-backend/db/seeds.rb
+++ AG-backend/db/seeds.rb
@@ -1,2 +1,5 @@
+Images.create(url: "https://unsplash.com/photos/RLLR0oRz16Y", description: "Photo by Boris Smokrovic, published on Nov. 24, 2016")
 
+Images.create(url: "https://unsplash.com/photos/DPXytK8Z59Y", description: "Photo by Boris Smokrovic, published on Feb. 6, 2017")
 
+Images.create(url: "https://unsplash.com/photos/gZlycYbRtkk", description: "Photo by Damon Hall, published on Feb. 12, 2019")
