6bee35762aa599f3206500be646fc07ef04d8850
bambi teague <teeegsa@gmail.com>
2021-08-20 04:58:53 -0500

    some back and forth, making sure I correctly set up the Votes table in my DB

--- AG-backend/db/migrate/20210820095730_create_votes.rb
+++ AG-backend/db/migrate/20210820095730_create_votes.rb
@@ -1,6 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[6.1]
   def change
     create_table :votes do |t|
+      t.boolean :upVote
+      t.boolean :downVote
 
       t.timestamps
     end
--- AG-backend/db/schema.rb
+++ AG-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_164212) do
+ActiveRecord::Schema.define(version: 2021_08_20_095730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2021_08_18_164212) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "imageUrl"
     t.text "description"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.boolean "upVote"
+    t.boolean "downVote"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
