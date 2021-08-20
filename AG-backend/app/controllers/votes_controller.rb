bf707359b133725e5209f7d9eebba5b184ced9ab
bambi teague <teeegsa@gmail.com>
2021-08-20 05:02:45 -0500

    remove unnecessary @'s from controller

--- AG-backend/app/controllers/votes_controller.rb
+++ AG-backend/app/controllers/votes_controller.rb
@@ -2,41 +2,41 @@ class VotesController < ApplicationController
   before_action :set_vote, only: [:show, :update, :destroy]
 
   def index
-    @votes = Vote.all
+    votes = Vote.all
 
-    render json: @votes
+    render json: votes
   end
 
   def show
-    render json: @vote
+    render json: vote
   end
 
   def create
-    @vote = Vote.new(vote_params)
+    vote = Vote.new(vote_params)
 
-    if @vote.save
-      render json: @vote, status: :created, location: @vote
+    if vote.save
+      render json: vote, status: :created, location: vote
     else
-      render json: @vote.errors, status: :unprocessable_entity
+      render json: vote.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if @vote.update(vote_params)
-      render json: @vote
+    if vote.update(vote_params)
+      render json: vote
     else
-      render json: @vote.errors, status: :unprocessable_entity
+      render json: vote.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @vote.destroy
+    vote.destroy
   end
 
   private
 
     def set_vote
-      @vote = Vote.find(params[:id])
+      vote = Vote.find(params[:id])
     end
 
 
