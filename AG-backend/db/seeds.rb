1f701a55b8e7a9cbe0ebf476a7d9e6d4e72df459
bambi teague <teeegsa@gmail.com>
2021-08-27 10:48:16 -0500

    reset/reseed the database with comments this time

--- AG-backend/db/seeds.rb
+++ AG-backend/db/seeds.rb
@@ -3,3 +3,21 @@ Post.create(imageUrl: "https://images.unsplash.com/photo-1480044965905-02098d419
 Post.create(imageUrl: "https://images.unsplash.com/photo-1486365227551-f3f90034a57c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80", description: "Photo by Boris Smokrovic, published on Feb. 6, 2017")
 
 Post.create(imageUrl: "https://images.unsplash.com/photo-1550030085-00cee362ae48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80", description: "Photo by Damon Hall, published on Feb. 12, 2019")
+
+Comment.create(post_id: "1", content: "What a lovely image!!")
+
+Comment.create(post_id: "1", content: "Fantastic shot! this looks awesome.")
+
+Comment.create(post_id: "2", content: "What a lovely image!!")
+
+Comment.create(post_id: "2", content: "Once in a lifetime shot, buddy!")
+
+Comment.create(post_id: "3", content: "My favorite bird. You captured it really well!")
+
+Comment.create(post_id: "3", content: "What a lovely image!!")
+
+Comment.create(post_id: "2", content: "I'm scared of most birds, but you have me loving this image")
+
+Comment.create(post_id: "1", content: "My favorite image so far")
+
+
