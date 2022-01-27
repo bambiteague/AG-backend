156a2e93e2392f2cbdc245fe88a1f0c53807c581
bambi teague <teeegsa@gmail.com>
2022-01-27 17:32:46 -0600

    adding files for set up

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,21 @@ function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      {console.log("butts")}
     </div>
   );
 }
 
 export default App;
 
-// NOTES: 
+// NOTES:
 // will need components for...
-  // submission form and related buttons
-  // displaying images after uploaded
-  // form for commenting on the image and related button(s)
-  // comments below appropriate image
+// submission form and related buttons
+// displaying images after uploaded
+// form for commenting on the image and related button(s)
+// comments below appropriate image
 
 // NEXT:
-  // create voting component with up and down vote buttons for each image
-  // add a user sign up/login/logout with username&password
-  // update comment component to show related username next to comment
-  // ability to like comments???? would be a nice bonus to add
+// create voting component with up and down vote buttons for each image
+// add a user sign up/login/logout with username&password
+// update comment component to show related username next to comment
+// ability to like comments???? would be a nice bonus to add
